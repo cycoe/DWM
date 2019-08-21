b2c18fa9f20cf2581a0644f39d589998ec67589e
cycoe <cycoe@cycoe.cc>
2019-08-21 19:00:50 +0800

    change colors

--- config.def.h
+++ config.def.h
@@ -8,15 +8,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=12" };
 static const char dmenufont[]       = "monospace:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_act_text[]    = "#eeeeee";
+static const char col_act_back[]    = "#339933";
+static const char col_act_bord[]    = "#4e3265";
+static const char col_ina_text[]    = "#bbbbbb";
+static const char col_ina_back[]    = "#4e3265";
+static const char col_ina_bord[]    = "#4e3265";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg            bg            border   */
+	[SchemeNorm] = { col_act_text, col_act_back, col_act_bord },
+	[SchemeSel]  = { col_ina_text, col_ina_back, col_ina_bord },
 };
 
 /* tagging */
